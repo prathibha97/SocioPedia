772143765c770804402ab23496e617913c14f23d
Prathibha Ratnayake <prsthibha@gmail.com>
2022-11-21 18:41:51 +0530

    Add user routes and controller

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import authRouter from './auth.route.js'
+import userRouter from './users.route.js'
 
 const api = express.Router()
 
 api.use('/auth', authRouter)
+api.use('/users', userRouter)
 
 export default api

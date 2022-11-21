8e1beef008665b782d5a70a84e753ca7737e64a8
Prathibha Ratnayake <prsthibha@gmail.com>
2022-11-21 13:00:53 +0530

    Add Ath-login route

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt'
 import User from '../models/User.js'
+import jwt from 'jsonwebtoken'
 
 export const register = async (req, res) => {
   try {
@@ -34,6 +35,23 @@ export const register = async (req, res) => {
     res.status(201).json(savedUser)
 
   } catch (err) {
-    register.status(500).json({ error: err.message })
+    res.status(500).json({ error: err.message })
+  }
+}
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body
+    const user = await User.findOne({ email: email })
+    if (!user) return res.status(404).json({ msg: 'User does not exist' })
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials' })
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+    delete user.password
+    res.status(200).json({ token, user })
+
+  } catch (err) {
+    res.status(500).json({ error: err.message })
   }
 }
--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { upload } from '../../services/file-storage.js'
-import { register } from '../controllers/auth.controller.js'
+import { login, register } from '../controllers/auth.controller.js'
 
 const authRouter = express.Router()
 
 authRouter.post('/register', upload.single('picture'), register)
+authRouter.post('/login', login)
 
 export default authRouter
